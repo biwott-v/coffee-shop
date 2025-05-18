215958e8d278fa158738f941f517dbbac10061ef
biwott-v <victor.biwott@student.moringaschool.com>
2025-05-18 21:50:29 +0300

    adds an Order class

--- coffee.py
+++ coffee.py
@@ -1,12 +1,12 @@
 class Coffee:
-    def __init__(self,name):
-        self._name=name
-    def set_name(name):
-        name = str(input("Enter a coffee name"))
-        if len(name) >= 3:
-            self_name=name
+    def __init__(self,coffee_name):
+        self._coffee_name=coffee_name
+    def set_coffee_name(coffee_name):
+        coffee_name = str(input("Enter a coffee name"))
+        if len(coffee_name) >= 3:
+            self._coffee_name=coffee_name
         else :
             return "Invalid coffee name (name must have at least than 3 characters)"
-    def get_name():
-        return self._name
-    name = property(get_name,set_name)
+    def get_coffee_name():
+        return self._coffee_name
+    coffee_name = property(get_coffee_name,set_coffee_name)
--- order.py
+++ order.py
@@ -1,3 +1,14 @@
-class Order:
-    def __init__(self):
-        pass
+from customer.py import Customer
+from coffee.py import Coffee 
+
+class Order(Customer,Coffee):
+    def __init__(self,price ):
+        Customer.__init__(self,name)
+        Coffee.__init__(self,coffee_name)
+        self._price = price
+    def set_price(self,price):
+        price = float(input("Enter the price  "))
+        if price <10 and price >1:
+            self._price = price
+        else :
+            return "Invalid price (Price should be between 1 and 10"
