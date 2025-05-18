7d47a2afc6cfe14c09885a441dced80b2ac8c53a
biwott-v <victor.biwott@student.moringaschool.com>
2025-05-18 21:33:16 +0300

    adds a Coffee class

--- coffee.py
+++ coffee.py
@@ -1,2 +1,12 @@
 class Coffee:
-    pass
+    def __init__(self,name):
+        self._name=name
+    def set_name(name):
+        name = str(input("Enter a coffee name"))
+        if len(name) >= 3:
+            self_name=name
+        else :
+            return "Invalid coffee name (name must have at least than 3 characters)"
+    def get_name():
+        return self._name
+    name = property(get_name,set_name)
