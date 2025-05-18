509ea0c68b1371824b243fb27c2a0ffc9e0e8eb3
biwott-v <victor.biwott@student.moringaschool.com>
2025-05-18 21:28:35 +0300

    adds a Customer class

---
+++ customer.py
@@ -0,0 +1,14 @@
+class Customer:
+    def __init__(name,self):
+        self._name=name
+    def set_name(self,name):
+        name=str(input("Kindly enter your name   "))
+        if (len(name)<=15 and len(name)>1):
+            self._name = name
+        else :
+            return ("Invalid name (name must be a between 1 and 15 characters)")
+    def get_name(self):
+        return self._name
+    name = property(set_name,get_name)
+
+victor = Customer("victor")
