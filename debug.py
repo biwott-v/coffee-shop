97e8ae048252950f2993f8b935e7f7b721434a57
biwott-v <victor.biwott@student.moringaschool.com>
2025-05-19 19:14:34 +0300

    adds a new_order function

--- debug.py
+++ debug.py
@@ -2,3 +2,12 @@ from customer import Customer
 from order import Order
 from coffee import Coffee
 
+#A new Order
+def new_order(my_order):
+    my_order = Order()
+    return my_order
+
+
+if __name__ == "__main__":
+    guest=""
+    print(new_order(guest))
